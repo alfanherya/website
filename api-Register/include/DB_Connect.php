a3842e01248d48e7477ba550f889eb73df79432b
alfanherya <alfanherya@gmail.com>
2020-12-23 16:58:28 +0700

    initial commit

---
+++ api-Register/include/DB_Connect.php
@@ -0,0 +1,16 @@
+<?php
+class DB_Connect{
+    private $conn;
+
+    // connecting to database
+    public function connect(){
+        require_once 'include/Config.php';
+
+        //connecting to mysql database
+        $this -> conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE) or die ("Database MYSQL Tidak terhubung");
+
+        //return database handler
+        return $this-> conn;
+    }
+}
+?>
